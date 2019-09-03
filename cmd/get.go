bdcdbd817ae78df7a50754465fd62414716eaad3
iwilltry42 <iwilltry42@gmail.com>
2019-09-03 12:04:02 +0200

    init cobra

---
+++ cmd/get.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2019 Thorsten Klein <iwilltry42@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -1,254 +1,28 @@
+/*
+Copyright © 2019 Thorsten Klein <iwilltry42@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
+import "github.com/rancher/k3d/cmd"
 
-	run "github.com/rancher/k3d/cli"
-	"github.com/rancher/k3d/version"
-	"github.com/urfave/cli"
-)
-
-// defaultK3sImage specifies the default image being used for server and workers
-const defaultK3sImage = "docker.io/rancher/k3s"
-const defaultK3sClusterName string = "k3s-default"
-
-// main represents the CLI application
 func main() {
-
-	// App Details
-	app := cli.NewApp()
-	app.Name = "k3d"
-	app.Usage = "Run k3s in Docker!"
-	app.Version = version.GetVersion()
-	app.Authors = []cli.Author{
-		{
-			Name:  "Thorsten Klein",
-			Email: "iwilltry42@gmail.com",
-		},
-		{
-			Name:  "Rishabh Gupta",
-			Email: "r.g.gupta@outlook.com",
-		},
-		{
-			Name: "Darren Shepherd",
-		},
-	}
-
-	// commands that you can execute
-	app.Commands = []cli.Command{
-		{
-			// check-tools verifies that docker is up and running
-			Name:    "check-tools",
-			Aliases: []string{"ct"},
-			Usage:   "Check if docker is running",
-			Action:  run.CheckTools,
-		},
-		{
-			// shell starts a shell in the context of a running cluster
-			Name:  "shell",
-			Usage: "Start a subshell for a cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "Set a name for the cluster",
-				},
-				cli.StringFlag{
-					Name:  "command, c",
-					Usage: "Run a shell command in the context of the cluster",
-				},
-				cli.StringFlag{
-					Name:  "shell, s",
-					Value: "auto",
-					Usage: "which shell to use. One of [auto, bash, zsh]",
-				},
-			},
-			Action: run.Shell,
-		},
-		{
-			// create creates a new k3s cluster in docker containers
-			Name:    "create",
-			Aliases: []string{"c"},
-			Usage:   "Create a single- or multi-node k3s cluster in docker containers",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "Set a name for the cluster",
-				},
-				cli.StringSliceFlag{
-					Name:  "volume, v",
-					Usage: "Mount one or more volumes into every node of the cluster (Docker notation: `source:destination`)",
-				},
-				cli.StringSliceFlag{
-					Name:  "publish, add-port",
-					Usage: "Publish k3s node ports to the host (Format: `[ip:][host-port:]container-port[/protocol]@node-specifier`, use multiple options to expose more ports)",
-				},
-				cli.IntFlag{
-					Name:  "port-auto-offset",
-					Value: 0,
-					Usage: "Automatically add an offset (* worker number) to the chosen host port when using `--publish` to map the same container-port from multiple k3d workers to the host",
-				},
-				cli.StringFlag{
-					// TODO: to be deprecated
-					Name:  "version",
-					Usage: "Choose the k3s image version",
-				},
-				cli.StringFlag{
-					// TODO: only --api-port, -a soon since we want to use --port, -p for the --publish/--add-port functionality
-					Name:  "api-port, a, port, p",
-					Value: "6443",
-					Usage: "Specify the Kubernetes cluster API server port (Format: `[host:]port` (Note: --port/-p will be used for arbitrary port mapping as of v2.0.0, use --api-port/-a instead for setting the api port)",
-				},
-				cli.IntFlag{
-					Name:  "wait, t",
-					Value: 0, // timeout
-					Usage: "Wait for the cluster to come up before returning until timoout (in seconds). Use --wait 0 to wait forever",
-				},
-				cli.StringFlag{
-					Name:  "image, i",
-					Usage: "Specify a k3s image (Format: <repo>/<image>:<tag>)",
-					Value: fmt.Sprintf("%s:%s", defaultK3sImage, version.GetK3sVersion()),
-				},
-				cli.StringSliceFlag{
-					Name:  "server-arg, x",
-					Usage: "Pass an additional argument to k3s server (new flag per argument)",
-				},
-				cli.StringSliceFlag{
-					Name:  "agent-arg",
-					Usage: "Pass an additional argument to k3s agent (new flag per argument)",
-				},
-				cli.StringSliceFlag{
-					Name:  "env, e",
-					Usage: "Pass an additional environment variable (new flag per variable)",
-				},
-				cli.IntFlag{
-					Name:  "workers, w",
-					Value: 0,
-					Usage: "Specify how many worker nodes you want to spawn",
-				},
-				cli.BoolFlag{
-					Name:  "auto-restart",
-					Usage: "Set docker's --restart=unless-stopped flag on the containers",
-				},
-			},
-			Action: run.CreateCluster,
-		},
-		{
-			// delete deletes an existing k3s cluster (remove container and cluster directory)
-			Name:    "delete",
-			Aliases: []string{"d", "del"},
-			Usage:   "Delete cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "name of the cluster",
-				},
-				cli.BoolFlag{
-					Name:  "all, a",
-					Usage: "Delete all existing clusters (this ignores the --name/-n flag)",
-				},
-			},
-			Action: run.DeleteCluster,
-		},
-		{
-			// stop stopy a running cluster (its container) so it's restartable
-			Name:  "stop",
-			Usage: "Stop cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "Name of the cluster",
-				},
-				cli.BoolFlag{
-					Name:  "all, a",
-					Usage: "Stop all running clusters (this ignores the --name/-n flag)",
-				},
-			},
-			Action: run.StopCluster,
-		},
-		{
-			// start restarts a stopped cluster container
-			Name:  "start",
-			Usage: "Start a stopped cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "Name of the cluster",
-				},
-				cli.BoolFlag{
-					Name:  "all, a",
-					Usage: "Start all stopped clusters (this ignores the --name/-n flag)",
-				},
-			},
-			Action: run.StartCluster,
-		},
-		{
-			// list prints a list of created clusters
-			Name:    "list",
-			Aliases: []string{"ls", "l"},
-			Usage:   "List all clusters",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "all, a",
-					Usage: "Also show non-running clusters",
-				},
-			},
-			Action: run.ListClusters,
-		},
-		{
-			// get-kubeconfig grabs the kubeconfig from the cluster and prints the path to it
-			Name:  "get-kubeconfig",
-			Usage: "Get kubeconfig location for cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n",
-					Value: defaultK3sClusterName,
-					Usage: "Name of the cluster",
-				},
-				cli.BoolFlag{
-					Name:  "all, a",
-					Usage: "Get kubeconfig for all clusters (this ignores the --name/-n flag)",
-				},
-			},
-			Action: run.GetKubeConfig,
-		},
-		{
-			// get-kubeconfig grabs the kubeconfig from the cluster and prints the path to it
-			Name:    "import-images",
-			Aliases: []string{"i"},
-			Usage:   "Import a comma- or space-separated list of container images from your local docker daemon into the cluster",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "name, n, cluster, c",
-					Value: defaultK3sClusterName,
-					Usage: "Name of the cluster",
-				},
-				cli.BoolFlag{
-					Name:  "no-remove, no-rm, keep, k",
-					Usage: "Disable automatic removal of the tarball",
-				},
-			},
-			Action: run.ImportImage,
-		},
-	}
-
-	// Global flags
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "verbose",
-			Usage: "Enable verbose output",
-		},
-	}
-
-	// run the whole thing
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
