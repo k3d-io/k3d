af4960aa33b3bab4117db01271e9c618c8440e4e
iwilltry42 <iwilltry42@gmail.com>
2019-09-04 21:03:41 +0200

    add start and stop packages

---
+++ cmd/start/start.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2019 Thorsten Klein <iwilltry42@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package start
+
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+)
+
+// NewCmdStart returns a new cobra command
+func NewCmdStart() *cobra.Command {
+
+	// create new cobra command
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start a resource.",
+		Long:  `Start a resource.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Debugln("start called")
+		},
+	}
+
+	// add subcommands
+	cmd.AddCommand(NewCmdStartCluster())
+	cmd.AddCommand(NewCmdStartNode())
+
+	// done
+	return cmd
+}
---
+++ cmd/stop/stopNode.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2019 Thorsten Klein <iwilltry42@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package stop
+
+import (
+	"github.com/spf13/cobra"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// NewCmdStopNode returns a new cobra command
+func NewCmdStopNode() *cobra.Command {
+
+	// create new command
+	cmd := &cobra.Command{
+		Use:   "node",
+		Short: "Stop an existing k3d node",
+		Long:  `Stop an existing k3d node.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Debugln("stop node called")
+		},
+	}
+
+	// done
+	return cmd
+}
