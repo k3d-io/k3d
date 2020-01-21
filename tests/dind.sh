2db715c064b1d4e933baef1b1496964f0ec161a1
Alvaro Saurin <alvaro.saurin@gmail.com>
2020-01-21 11:28:38 +0100

    Stop the DinD e2e runner when done
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- tests/dind.sh
+++ tests/dind.sh
@@ -1,15 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
+K3D_EXE=${EXE:-/bin/k3d}
 K3D_IMAGE_TAG=$1
 
-k3de2e=$(docker run -d --rm \
+# define E2E_KEEP to non-empty for keeping the e2e runner container after running the tests
+E2E_KEEP=${E2E_KEEP:-}
+
+####################################################################################
+
+TIMESTAMP=$(date "+%m%d%H%M%S")
+
+k3de2e=$(docker run -d \
           -v "$(pwd)"/tests:/tests \
           --privileged \
-          -e EXE="/bin/k3d" \
+          -e EXE="$K3D_EXE" \
           -e CI="true" \
+          --name "k3d-e2e-runner-$TIMESTAMP" \
           k3d:$K3D_IMAGE_TAG)
 
 sleep 5 # wait 5 seconds for docker to start
 
 # Execute tests
-docker exec $k3de2e /tests/runner.sh
+finish() {
+  docker stop "$k3de2e" || /bin/true
+  if [ -z "$E2E_KEEP" ] ; then
+    docker rm "$k3de2e" || /bin/true
+  fi
+}
+trap finish EXIT
+
+docker exec "$k3de2e" /tests/runner.sh
