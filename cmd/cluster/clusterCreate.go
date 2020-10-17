ffe8c9697c530b4f8f0fda97e5f068316e1fe7b2
Lionel Nicolas <lionel.nicolas@nividic.org>
2020-10-16 20:46:04 -0400

    add ability to set container labels on nodes using --label/-l
    This supports node filters as is already done for '--volume' and '--port'.

--- cmd/cluster/clusterCreate.go
+++ cmd/cluster/clusterCreate.go
@@ -128,6 +128,7 @@ func NewCmdClusterCreate() *cobra.Command {
 	cmd.Flags().String("token", "", "Specify a cluster token. By default, we generate one.")
 	cmd.Flags().StringArrayP("volume", "v", nil, "Mount volumes into the nodes (Format: `[SOURCE:]DEST[@NODEFILTER[;NODEFILTER...]]`\n - Example: `k3d cluster create --agents 2 -v \"/my/path@agent[0,1]\" -v \"/tmp/test:/tmp/other@server[0]\"`")
 	cmd.Flags().StringArrayP("port", "p", nil, "Map ports from the node containers to the host (Format: `[HOST:][HOSTPORT:]CONTAINERPORT[/PROTOCOL][@NODEFILTER]`)\n - Example: `k3d cluster create --agents 2 -p \"8080:80@agent[0]\" -p \"8081@agent[1]\"`")
+	cmd.Flags().StringArrayP("label", "l", nil, "Add label to node container (Format: `KEY[=VALUE][@NODEFILTER[;NODEFILTER...]]`\n - Example: `k3d cluster create --agents 2 -l \"my.label@agent[0,1]\" -v \"other.label=somevalue@server[0]\"`")
 	cmd.Flags().BoolVar(&createClusterOpts.WaitForServer, "wait", true, "Wait for the server(s) to be ready before returning. Use '--timeout DURATION' to not wait forever.")
 	cmd.Flags().DurationVar(&createClusterOpts.Timeout, "timeout", 0*time.Second, "Rollback changes if cluster couldn't be created in specified duration.")
 	cmd.Flags().BoolVar(&updateDefaultKubeconfig, "update-default-kubeconfig", true, "Directly update the default kubeconfig with the new cluster's context")
@@ -317,6 +318,32 @@ func parseCreateClusterCmd(cmd *cobra.Command, args []string, createClusterOpts 
 
 	log.Tracef("PortFilterMap: %+v", portFilterMap)
 
+	// --label
+	labelFlags, err := cmd.Flags().GetStringArray("label")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// labelFilterMap will add container label to applied node filters
+	labelFilterMap := make(map[string][]string, 1)
+	for _, labelFlag := range labelFlags {
+
+		// split node filter from the specified label
+		label, filters, err := cliutil.SplitFiltersFromFlag(labelFlag)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// create new entry or append filter to existing entry
+		if _, exists := labelFilterMap[label]; exists {
+			labelFilterMap[label] = append(labelFilterMap[label], filters...)
+		} else {
+			labelFilterMap[label] = filters
+		}
+	}
+
+	log.Tracef("LabelFilterMap: %+v", labelFilterMap)
+
 	/********************
 	 *									*
 	 * generate cluster *
@@ -408,6 +435,23 @@ func parseCreateClusterCmd(cmd *cobra.Command, args []string, createClusterOpts 
 		}
 		for _, node := range nodes {
 			node.Ports = append(node.Ports, portmap)
+		}
+	}
+
+	// append labels
+	for label, filters := range labelFilterMap {
+		nodes, err := cliutil.FilterNodes(cluster.Nodes, filters)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for _, node := range nodes {
+			// ensure node.Labels map is initialized (see also ClusterCreate.nodeSetup)
+			if node.Labels == nil {
+				node.Labels = make(map[string]string)
+			}
+
+			labelKey, labelValue := cliutil.SplitLabelKeyValue(label)
+			node.Labels[labelKey] = labelValue
 		}
 	}
 
