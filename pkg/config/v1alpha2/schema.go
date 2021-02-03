e2c93e9bdb5ce752980a6c4d1dae0c11fcc37292
iwilltry42 <iwilltry42@gmail.com>
2021-02-03 08:54:46 +0100

    improve schema

--- pkg/config/v1alpha2/schema.go
+++ pkg/config/v1alpha2/schema.go
@@ -1,4 +1,32 @@
-{
+/*
+Copyright © 2020 The k3d Author(s)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package v1alpha2
+
+// JSONSchema describes the schema used to validate config files
+/* TODO: JSONSchema should be an embedded file. We're moving to the //go:embed tag as of Go 1.16
+ * ... and only hardcode it here to avoid using 3rd party tools like go-bindata or packr right now for the time being
+ */
+var JSONSchema = `{
   "$schema": "http://json-schema.org/draft-07/schema#",
   "title": "SimpleConfig",
   "type": "object",
@@ -206,4 +234,4 @@
       }
     }
   }
-}
+}`
