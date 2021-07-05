4b1b14034c66955f21a7c7fdf881ac0b71d7d673
iwilltry42 <iwilltry42@gmail.com>
2021-07-05 19:04:20 +0200

    simplify cuda guide

--- docs/usage/guides/cuda/build.sh
+++ docs/usage/guides/cuda/build.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
-export CI_REGISTRY_IMAGE="YOUR_REGISTRY_IMAGE_URL"
-export VERSION="1.0"
-export K3S_TAG="v1.21.2+k3s1"
-export DOCKER_VERSION="20.10.7"
-export IMAGE_TAG="v1.21.2-k3s1"
-export NVIDIA_CONTAINER_RUNTIME_VERSION="3.5.0-1"
+set -euxo pipefail
 
-docker build -f Dockerfile.base --build-arg DOCKER_VERSION=$DOCKER_VERSION -t $CI_REGISTRY_IMAGE/base:$VERSION . && \
-docker push $CI_REGISTRY_IMAGE/base:$VERSION
+K3S_TAG=${K3S_TAG:="v1.21.2-k3s1"} # replace + with -, if needed
+IMAGE_REGISTRY=${IMAGE_REGISTRY:="MY_REGISTRY"}
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:="rancher/k3s"}
+IMAGE_TAG="$K3S_TAG-cuda"
+IMAGE=${IMAGE:="$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_TAG"}
 
-rm -rf ./k3s && \
-git clone --depth 1 https://github.com/rancher/k3s.git -b "$K3S_TAG" && \
-docker run -ti -v ${PWD}/k3s:/k3s -v /var/run/docker.sock:/var/run/docker.sock $CI_REGISTRY_IMAGE/base:1.0 sh -c "cd /k3s && make" && \
-ls -al k3s/build/out/data.tar.zst
+NVIDIA_CONTAINER_RUNTIME_VERSION=${NVIDIA_CONTAINER_RUNTIME_VERSION:="3.5.0-1"}
 
-if [ -f k3s/build/out/data.tar.zst ]; then
-  echo "File exists! Building!"
-  docker build -f Dockerfile.k3d-gpu \
-    --build-arg NVIDIA_CONTAINER_RUNTIME_VERSION=$NVIDIA_CONTAINER_RUNTIME_VERSION\
-    -t $CI_REGISTRY_IMAGE:$IMAGE_TAG . && \
-  docker push $CI_REGISTRY_IMAGE:$IMAGE_TAG
-  echo "Done!"
-else
-  echo "Error, file does not exist!"
-  exit 1
-fi
+echo "IMAGE=$IMAGE"
 
-docker build -t $CI_REGISTRY_IMAGE:$IMAGE_TAG .
+# due to some unknown reason, copying symlinks fails with buildkit enabled
+DOCKER_BUILDKIT=0 docker build \
+  --build-arg K3S_TAG=$K3S_TAG \
+  --build-arg NVIDIA_CONTAINER_RUNTIME_VERSION=$NVIDIA_CONTAINER_RUNTIME_VERSION \
+  -t $IMAGE .
+docker push $IMAGE
+echo "Done!"
