58039daf63935ff359e57b42af22b0425c45ecd4
Valerii <vainkop@gmail.com>
2021-07-05 15:29:01 +0300

    Make nvidia container runtime version configurable & remove sudo in build.sh

--- docs/usage/guides/cuda/build.sh
+++ docs/usage/guides/cuda/build.sh
@@ -5,19 +5,22 @@ export VERSION="1.0"
 export K3S_TAG="v1.21.2+k3s1"
 export DOCKER_VERSION="20.10.7"
 export IMAGE_TAG="v1.21.2-k3s1"
+export NVIDIA_CONTAINER_RUNTIME_VERSION="3.5.0-1"
 
-sudo docker build -f Dockerfile.base --build-arg DOCKER_VERSION=$DOCKER_VERSION -t $CI_REGISTRY_IMAGE/base:$VERSION . && \
-sudo docker push $CI_REGISTRY_IMAGE/base:$VERSION
+docker build -f Dockerfile.base --build-arg DOCKER_VERSION=$DOCKER_VERSION -t $CI_REGISTRY_IMAGE/base:$VERSION . && \
+docker push $CI_REGISTRY_IMAGE/base:$VERSION
 
-sudo rm -rf ./k3s && \
+rm -rf ./k3s && \
 git clone --depth 1 https://github.com/rancher/k3s.git -b "$K3S_TAG" && \
-sudo docker run -ti -v ${PWD}/k3s:/k3s -v /var/run/docker.sock:/var/run/docker.sock $CI_REGISTRY_IMAGE/base:1.0 sh -c "cd /k3s && make" && \
-sudo ls -al k3s/build/out/data.tar.zst
+docker run -ti -v ${PWD}/k3s:/k3s -v /var/run/docker.sock:/var/run/docker.sock $CI_REGISTRY_IMAGE/base:1.0 sh -c "cd /k3s && make" && \
+ls -al k3s/build/out/data.tar.zst
 
 if [ -f k3s/build/out/data.tar.zst ]; then
   echo "File exists! Building!"
-  sudo docker build -f Dockerfile.k3d-gpu -t $CI_REGISTRY_IMAGE:$IMAGE_TAG . && \
-  sudo docker push $CI_REGISTRY_IMAGE:$IMAGE_TAG
+  docker build -f Dockerfile.k3d-gpu \
+    --build-arg NVIDIA_CONTAINER_RUNTIME_VERSION=$NVIDIA_CONTAINER_RUNTIME_VERSION\
+    -t $CI_REGISTRY_IMAGE:$IMAGE_TAG . && \
+  docker push $CI_REGISTRY_IMAGE:$IMAGE_TAG
   echo "Done!"
 else
   echo "Error, file does not exist!"
