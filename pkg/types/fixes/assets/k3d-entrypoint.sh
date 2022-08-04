3d05733c508e804dc3b17379384bb93e3a4d9ceb
Arik Maor <arikmaor@gmail.com>
2022-08-04 15:12:30 +0300

    Drain node before container stops

--- pkg/types/fixes/assets/k3d-entrypoint.sh
+++ pkg/types/fixes/assets/k3d-entrypoint.sh
@@ -16,4 +16,22 @@ done
 
 echo "[$(date -Iseconds)] Finished k3d entrypoint scripts!" >> "$LOGFILE"
 
-exec /bin/k3s "$@"
+/bin/k3s "$@" &
+k3s_pid=$!
+
+until kubectl uncordon $HOSTNAME; do sleep 3; done
+
+function cleanup() {
+  echo Draining node...
+  kubectl drain $HOSTNAME --force --delete-emptydir-data
+  echo Sending SIGTERM to k3s...
+  kill -15 $k3s_pid
+  echo Waiting for k3s to close...
+  wait $k3s_pid
+  echo Bye!
+}
+
+trap cleanup SIGTERM SIGINT SIGQUIT SIGHUP
+
+wait $k3s_pid
+echo Bye!
