bb57b32a4341291ca001b6c427148e4756ffdbfb
iwilltry42 <tk@thklein.io>
2023-02-08 09:37:28 +0100

    add: additional check steps for regisdtration url test

--- tests/local_test_registration_using_server_lb.sh
+++ tests/local_test_registration_using_server_lb.sh
@@ -16,7 +16,7 @@ clean_up() {
 check_k3s_url() {
   local node=$1
   local expected_url=$2
-  local curr_url=$(docker inspect $node | grep K3S_URL | awk -F"=" '{print $2}' | sed 's/",//')
+  local curr_url=$(docker inspect "$node" | grep K3S_URL | awk -F"=" '{print $2}' | sed 's/",//')
 
   if [ "$curr_url" != "$expected_url" ]; then
     failed "Default K3S URL mismatch expected: '${expected_url}' got: ${curr_url}"
@@ -45,6 +45,7 @@ run_with_timeout() {
 
 # Constants for tests
 BIN=k3d
+export EXE=$BIN
 CLUSTER_NAME=test-lb-registration
 K3S_URL_DEFAULT="https://k3d-${CLUSTER_NAME}-server-0:6443"
 K3S_URL_LB="https://k3d-${CLUSTER_NAME}-serverlb:6443"
@@ -53,23 +54,34 @@ clean_up
 info "Starting a multi server cluster"
 run "$BIN cluster create $CLUSTER_NAME -s 3"
 
+check_multi_node $CLUSTER_NAME 3
+
 info "Adding an agent and checking its K3S_URL"
 run_with_timeout "$BIN node create -c $CLUSTER_NAME testagent"
 check_k3s_url k3d-testagent-0 $K3S_URL_DEFAULT
 
+check_multi_node $CLUSTER_NAME 4
+
 info "Deleting server-0"
 run_with_timeout "$BIN node delete k3d-${CLUSTER_NAME}-server-0"
 run "kubectl delete node k3d-${CLUSTER_NAME}-server-0"
+
+check_multi_node $CLUSTER_NAME 3
 
 info "Adding a new agent to check the new K3S_URL"
 run_with_timeout "$BIN node create -c $CLUSTER_NAME testagent1"
 check_k3s_url k3d-testagent1-0 $K3S_URL_LB
 run_with_timeout "$BIN node delete k3d-testagent1-0"
 
+check_multi_node $CLUSTER_NAME 4
+
 info "Adding a new server to check K3S_URL"
 run_with_timeout "$BIN node create -c $CLUSTER_NAME testserver --role server"
 check_k3s_url k3d-testserver-0 $K3S_URL_LB
 run_with_timeout "$BIN node delete k3d-testserver-0"
+
+check_multi_node $CLUSTER_NAME 5
+
 
 clean_up
 
