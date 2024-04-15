e9babb74413c46fa89a4e61e738121a6d53a86c9
Danny Breyfogle <dbreyfogle@users.noreply.github.com>
2024-04-14 22:10:57 -0700

    Update CUDA docs to use k3s suggested method (#1430)

--- docs/usage/advanced/cuda/build.sh
+++ docs/usage/advanced/cuda/build.sh
@@ -2,20 +2,18 @@
 
 set -euxo pipefail
 
-K3S_TAG=${K3S_TAG:="v1.21.2-k3s1"} # replace + with -, if needed
+K3S_TAG=${K3S_TAG:="v1.28.8-k3s1"} # replace + with -, if needed
+CUDA_TAG=${CUDA_TAG:="12.4.1-base-ubuntu22.04"}
 IMAGE_REGISTRY=${IMAGE_REGISTRY:="MY_REGISTRY"}
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:="rancher/k3s"}
-IMAGE_TAG="$K3S_TAG-cuda"
+IMAGE_TAG="$K3S_TAG-cuda-$CUDA_TAG"
 IMAGE=${IMAGE:="$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_TAG"}
-
-NVIDIA_CONTAINER_RUNTIME_VERSION=${NVIDIA_CONTAINER_RUNTIME_VERSION:="3.5.0-1"}
 
 echo "IMAGE=$IMAGE"
 
-# due to some unknown reason, copying symlinks fails with buildkit enabled
-DOCKER_BUILDKIT=0 docker build \
+docker build \
   --build-arg K3S_TAG=$K3S_TAG \
-  --build-arg NVIDIA_CONTAINER_RUNTIME_VERSION=$NVIDIA_CONTAINER_RUNTIME_VERSION \
+  --build-arg CUDA_TAG=$CUDA_TAG \
   -t $IMAGE .
 docker push $IMAGE
 echo "Done!"
