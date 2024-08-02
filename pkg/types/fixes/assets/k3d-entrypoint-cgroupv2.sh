60695db835ea8d3f0f6c95cc320a068d8aa5c44d
Dejan Benedik <thejan2009@users.noreply.github.com>
2024-08-02 12:59:38 +0200

    fix: busybox xargs unlinked on new k3s releases (#1479)

--- pkg/types/fixes/assets/k3d-entrypoint-cgroupv2.sh
+++ pkg/types/fixes/assets/k3d-entrypoint-cgroupv2.sh
@@ -11,10 +11,15 @@ set -o nounset
 #########################################################################################################################################
 if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
   echo "[$(date -Iseconds)] [CgroupV2 Fix] Evacuating Root Cgroup ..."
-	# move the processes from the root group to the /init group,
+  # move the processes from the root group to the /init group,
   # otherwise writing subtree_control fails with EBUSY.
   mkdir -p /sys/fs/cgroup/init
-  busybox xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+  # new k3s releases only have xargs from findutils
+  if command -v xargs >/dev/null; then
+    xargs -rn1 </sys/fs/cgroup/cgroup.procs >/sys/fs/cgroup/init/cgroup.procs || :
+  else
+    busybox xargs -rn1 </sys/fs/cgroup/cgroup.procs >/sys/fs/cgroup/init/cgroup.procs || :
+  fi
   # enable controllers
   sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
   echo "[$(date -Iseconds)] [CgroupV2 Fix] Done"
